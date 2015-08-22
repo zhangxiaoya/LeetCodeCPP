79ec01a50d7450819f5b59ec0e25dc9484d36976
zhangxiaoya <zhangxiaoya1989@gmail.com>
2015-08-23 00:24:37 +0800

    Lowest Common Ancestor of a Binary Tree

--- solution236.cpp
+++ solution236.cpp
@@ -5,3 +5,41 @@ Solution236::Solution236()
 
 }
 
+bool GetPath(TreeNode* root,TreeNode* p,vector<TreeNode*>& path)
+{
+    if(root == NULL)
+        return false;
+    path.push_back(root);
+    if(root == p)
+        return true;
+
+    if(!GetPath(root->left,p,path) && !GetPath(root->right,p,path))
+    {
+        path.pop_back();
+        return false;
+    }
+}
+
+TreeNode* GetLast(vector<TreeNode*> path1,vector<TreeNode*> path2)
+{
+    TreeNode* last;
+    vector<TreeNode*>::iterator it1,it2;
+    it1 = path1.begin();
+    it2 = path2.begin();
+    while(it1 != path1.end() && it2 != path2.end())
+    {
+        if(*it1 == *it2)
+            last = *it1;
+        it1++;
+        it2++;
+    }
+    return last;
+}
+
+TreeNode* Solution236::lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+{
+    vector<TreeNode*> path1,path2;
+    GetPath(root,p,path1);
+    GetPath(root,q,path2);
+    return GetLast(path1,path2);
+}
--- solution236.h
+++ solution236.h
@@ -1,11 +1,24 @@
 #ifndef SOLUTION236_H
 #define SOLUTION236_H
 
+#define NULL nullptr
+
+#include <vector>
+using std::vector;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
 
 class Solution236
 {
 public:
     Solution236();
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q);
 };
 
 #endif // SOLUTION236_H

99d9763662a8efabed5be239f003f1ffed783d48
zhangxiaoya <zhangxiaoya1989@gmail.com>
2015-09-20 00:33:37 +0800

    remove zeros

--- solution283.cpp
+++ solution283.cpp
@@ -10,10 +10,6 @@ void Solution283::moveZeroes(vector<int> &nums)
     int n = nums.size();
     if(n <1)
         return;
-//    int count = 0;
-//    for(int i = 0;i<n;++i)
-//        if(nums[i] == 0)
-//            count++;
     int i =0,j =0;
     while(j<n)
     {

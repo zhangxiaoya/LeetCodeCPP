a5ce329dd9f9903b594a598f844c382c865f0951
zhangxiaoya <tiyijushi@163.com>
2017-02-04 10:09:19 +0800

    remove help method for 198

--- solution198.cpp
+++ solution198.cpp
@@ -4,22 +4,18 @@ Solution198::Solution198()
 {
 }
 
-int Solution198::help(int index, vector<int>& nums){
-    if(index < 0){
-        return 0;
-    }
-    if(result[index] == -1){
-        result[index] = max(nums[index] + help(index-2, nums), help(index-1,nums));
-    }
-    return result[index];
-}
 int Solution198::rob(vector<int>& nums) {
     if(nums.size() == 0){
         return 0;
     }
-    result = new int[nums.size()];
-    for(int i =0;i< nums.size();++i){
-        result[i] = -1;
+    if(nums.size() == 1){
+        return nums[0];
     }
-    return help(nums.size()-1,nums);
+    result = new int[nums.size()];
+    result[0] = nums[0];
+    result[1] = max(nums[1],nums[0]);
+    for(int i =2;i< nums.size();++i){
+        result[i] = max(nums[i] + result[i-2], result[i-1]);
+    }
+    return result[nums.size()-1];
 }

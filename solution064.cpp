baf66f0ffedba6b0925174676baf02528e1ae30a
zhangxiaoya <tiyijushi@163.com>
2017-02-06 00:48:12 +0800

    impove space cost

--- solution064.cpp
+++ solution064.cpp
@@ -7,23 +7,23 @@ Solution064::Solution064()
 int Solution064::minPathSum(vector<vector<int> > &grid){
     int m = grid.size();
     int n = grid[0].size();
-    vector<vector<int> > dp(m, vector<int>(n));
+    vector<int> dp(n);
 
     for( int i=0;i<m;++i){
         for(int j=0;j<n;++j){
             if(i==0 && j==0){
-                dp[i][j] =  grid[i][j];
+                dp[j] =  grid[i][j];
             }
             else if(i ==0){
-                dp[i][j] = dp[i][j-1] + grid[i][j];
+                dp[j] = dp[j-1] + grid[i][j];
             }
             else if(j ==0){
-                dp[i][j] = dp[i-1][j] + grid[i][j];
+                dp[j] = dp[j] + grid[i][j];
             }
             else{
-                dp[i][j] = min(dp[i][j-1] , dp[i-1][j]) + grid[i][j];
+                dp[j] = min(dp[j-1] , dp[j]) + grid[i][j];
             }
         }
     }
-    return dp[m-1][n-1];
+    return dp[n-1];
 }

ed270d00303db66c0cb7c72029df279187361df8
zhangxiaoya <zhangxiaoya1989@gmail.com>
2018-09-17 09:03:59 +0800

    solution 901

---
+++ LeetCodeCLion/Sort/Solution901/Solution901.cpp
@@ -0,0 +1,19 @@
+
+#include "Solution901.h"
+
+vector<int> Solution901::sortArrayByParity(vector<int> &A)
+{
+    if(A.size() > 1)
+    {
+        int beg = 0;
+        int end = A.size() -1;
+        while (beg < end)
+        {
+            while (beg < end && A[beg] % 2 == 0) beg++;
+            while(beg < end && A[end] %2 == 1) end --;
+            if(beg < end)
+                std::swap(A[beg], A[end]);
+        }
+    }
+    return A;
+}
---
+++ LeetCodeCLion/Sort/Solution901/Solution901.h
@@ -0,0 +1,15 @@
+
+#ifndef LEETCODECLION_SOLUTION901_H
+#define LEETCODECLION_SOLUTION901_H
+
+#include <vector>
+using std::vector;
+
+class Solution901
+{
+public:
+    vector<int> sortArrayByParity(vector<int>& A);
+};
+
+
+#endif //LEETCODECLION_SOLUTION901_H
